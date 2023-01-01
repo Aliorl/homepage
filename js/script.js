afec66d589742c11292e0aef8deec89ad55cdc81
Alicja Orlik <alicja.k.orlik@gmail.com>
2023-01-01 21:48:22 +0100

    changed css file and js file

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ let body = document.querySelector(".body");
 let themeName = document.querySelector(".theme__name");
 
 button.addEventListener("click", () => {
-    body.classList.toggle("section--dark");
+    body.classList.toggle("body--dark");
 
-    themeName.innerText = body.classList.contains("section--dark") ? "jasny" : "ciemny";
+    themeName.innerText = body.classList.contains("body--dark") ? "jasny" : "ciemny";
 });
